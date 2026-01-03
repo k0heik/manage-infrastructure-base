0bd2b448d4e6a00d939a193f7ffa5f7dcc2bfec5
k0heik <alcak1729@gmail.com>
2026-01-03 17:06:29 +0900

    Add IAM role policy attachment for GitHub Actions with AdministratorAccess

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -40,3 +40,8 @@ resource "aws_iam_role" "github_actions" {
   name               = "github-actions-oidc-role"
   assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
 }
+
+resource "aws_iam_role_policy_attachment" "github_actions_admin" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
