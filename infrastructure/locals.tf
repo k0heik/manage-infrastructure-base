6985618b8f30885c80c0309a6c050a5e4709afd3
k0heik <alcak1729@gmail.com>
2026-01-03 16:24:50 +0900

    Add default tags to AWS provider and define local variables

--- infrastructure/locals.tf
+++ infrastructure/locals.tf
@@ -1,1 +1,8 @@
-locals {}
+locals {
+  system = "manage-infrastructure-base"
+  Environment = var.environment
+  default_tags = {
+    Environment = var.environment
+    System      = local.system
+  }
+}
