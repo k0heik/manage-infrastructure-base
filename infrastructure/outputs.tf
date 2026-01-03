7748e2af766b610cf1520f9594bb7ed05d7e8df7
k0heik <alcak1729@gmail.com>
2026-01-03 16:01:04 +0900

    Remove unused S3 bucket and OIDC provider outputs; add missing data sources for GitHub Actions OIDC integration

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,11 +1,3 @@
-output "main_bucket_name" {
-  value = aws_s3_bucket.main.bucket
-}
-
-output "github_oidc_provider_arn" {
-  value = aws_iam_openid_connect_provider.github_actions.arn
-}
-
 output "github_actions_role_arn" {
   value = aws_iam_role.github_actions.arn
 }
